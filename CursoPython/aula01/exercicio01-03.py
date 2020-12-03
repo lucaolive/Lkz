9ce15e31af658f382c0bc0c6702a10db54cf8148
Lucas Alexandre <lucas_aodp@hotmail.com>
2020-12-03 13:33:57 +0000

    exercicio 01/03

--- CursoPython/aula01/exercicio01-03.py
+++ CursoPython/aula01/exercicio01-03.py
@@ -3,8 +3,8 @@ intnum = input("Write a number: ")
 if intnum.isnumeric():
     intnum = int(intnum)
     if intnum % 2 == 0:
-        print(f"O número {intnum} é par.")
+        print(f"The númber {intnum} is par.")
     else:
-        print(f"o número {intnum} é impar.")
+        print(f"The number {intnum} is impar.")
 else:
     print("Error, please write a number.")
