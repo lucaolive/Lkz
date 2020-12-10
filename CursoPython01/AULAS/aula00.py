6252951ba4b1110ab964e11e4089504f2ba55a41
Lucas Alexandre Oliveira de Paula <74148805+lucaolive@users.noreply.github.com>
2020-12-10 22:40:39 +0000

    work space

---
+++ CursoPython01/AULAS/aula00.py
@@ -0,0 +1,11 @@
+print("Linha 1")
+print("Linha 2")
+# Comentario para dizer oq isso significa
+print("Linha 3")
+print("Linha 4")  # Isso é um comentario
+print("Linha 5")
+"""print("Linha 6")
+print("Linha 6")
+print("Linha 6")
+print("Linha 6")"""
+print("Linha 7")
