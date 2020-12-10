226b15f735aecc86e4a383c5c2fd8a2d7f228944
Lucas Alexandre Oliveira de Paula <74148805+lucaolive@users.noreply.github.com>
2020-12-10 22:33:33 +0000

    work space

--- CursoPython01/AULAS/aula01.py
+++
@@ -1,8 +0,0 @@
-
-print(123456)
-print("Lucas", "Alexandre")
-print("Lucas", "Alexandre", sep="-")
-print("Lucas", "Alexandre", sep="-")
-print("824", "176", "070", sep=".", end="-")
-"""sep = separador | end = sibstitui a quebra de linha"""
-print("18")
--- CursoPython01/AULAS/aula02.py
+++
@@ -1,5 +0,0 @@
-"""
-str - string
-"""
-print("Essa é uma -string- (str).")
-print("aqui\nquebra a linha")  # \n quebra a linha
