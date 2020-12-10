e3156a54acb4253a391415a61bc751b16da2e22a
Lucas Alexandre Oliveira de Paula <74148805+lucaolive@users.noreply.github.com>
2020-12-10 23:00:56 +0000

    work space changement

--- CursoPython/aula01/aula04.py
+++
@@ -1,9 +0,0 @@
-"""
-+, -, *, /, //, **, %
-"""
-print("adiçao", 10 + 10)
-print("subtraçao", 10 - 10)
-print("divisao float", 10 / 10)
-print("divisao int", 10 // 10)
-print("potencia", 10 ** 10)
-print("modulo", 10 % 10)
--- CursoPython/aula01/exercicio03-03.py
+++
@@ -1,9 +0,0 @@
-nome = input("Write your name: ")
-qtd = nome.__len__()
-
-if qtd <= 4:
-    print("Seu nome é curto")
-elif qtd > 4 and qtd < 7:
-    print("Seu nome é normal")
-else:
-    print("Seu nome é muito grande")
