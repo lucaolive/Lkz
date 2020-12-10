0cfe12198aedddb67480c67018626ee4a4e2845a
Lucas Alexandre Oliveira de Paula <74148805+lucaolive@users.noreply.github.com>
2020-12-10 22:42:37 +0000

    print()

---
+++ CursoPython01/AULAS/aula01.py
@@ -0,0 +1,8 @@
+
+print(123456)
+print("Lucas", "Alexandre")
+print("Lucas", "Alexandre", sep="-")
+print("Lucas", "Alexandre", sep="-")
+print("824", "176", "070", sep=".", end="-")
+"""sep = separador | end = sibstitui a quebra de linha"""
+print("18")
