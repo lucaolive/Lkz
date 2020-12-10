6e0e75da5e9a13e249d12293d539c262e3be0535
Lucas Alexandre Oliveira de Paula <74148805+lucaolive@users.noreply.github.com>
2020-12-10 22:44:57 +0000

    tipos de dados

---
+++ CursoPython01/AULAS/aula03.py
@@ -0,0 +1,12 @@
+"""
+Tipos de Dados
+str - string | texto "....."
+int - inteiros | 0123456789 -012346789
+float - real/ponto flutuante | 10.50/1.5/-50.98/0.0
+bool - booleano/lógico | true/false 10 == 10
+"""
+print("Lucas", type("Lucas"))
+print(123456789, type(123456789))
+print(10.50, type(10.50))
+print(10 == 10, type(10 == 10))
+# "type" diz qual o tipo de dado
